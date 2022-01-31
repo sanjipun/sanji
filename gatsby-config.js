e2925b45f8fe437aee4fbc1224ca8490c43d1651
sanjipun <iamsanjipun@gmail.com>
2022-01-31 21:10:25 +0545

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,6 @@ module.exports = {
     },
     plugins: [
         'gatsby-plugin-postcss',
+        'gatsby-plugin-react-helmet'
     ],
 }
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,11 +2,6 @@ import React from 'react'
 import Layout from "./src/components/Layout"
 import "./src/styles/global.css"
 
-export const onRouteUpdate = ({ location, prevLocation }) => {
-    console.log("new pathname", location.pathname)
-    console.log("old pathname", prevLocation ? prevLocation.pathname : null)
-}
-
 export const wrapPageElement = ({ element }) => {
     return <Layout>{element}</Layout>
 }
