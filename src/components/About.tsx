6de1527dd9378d80db1d1fa46368cf3be782c1b9
sanjipun <iamsanjipun@gmail.com>
2022-01-31 21:10:30 +0545

    About

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="bg-black text-white">
+      <div className="max-w-8xl w-full mx-auto px-10 py-20">About ME</div>
+    </div>
+  );
+};
+
+export default About;
