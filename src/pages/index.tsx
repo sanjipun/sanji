689cd617a9788ce7a306ffb4137580502aa53500
sanjipun <iamsanjipun@gmail.com>
2022-01-31 21:11:01 +0545

    homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import About from '../components/About';
+import SEO from '../components/SEO';
 
 const IndexPage = () => {
   return (
-    <main>
-      <h1 className="text-3xl text-red-500">Hello world!</h1>
-    </main>
+    <section>
+      <SEO title="Sanji Pun | Frontend Engineer" />
+      <div className="max-w-8xl w-full mx-auto px-10">
+        <h1 className="text-3xl text-red-500">Hello world!</h1>
+      </div>
+      <About />
+    </section>
   );
 };
 
