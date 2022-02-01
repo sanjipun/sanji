dc0d8ad26f42e942c711988d731765a0fd2477b3
sanjipun <iamsanjipun@gmail.com>
2022-02-01 21:53:08 +0545

    update contact

---
+++ src/components/Contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="max-w-8xl mx-auto px-10 grid grid-cols-12 py-32">
+      <div className="col-span-12 sm:col-span-2">Contact</div>
+      <div className="col-span-12 sm:col-span-10 text-center text-4xl break-words">
+        iamsanjaypun@gmail.com
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
