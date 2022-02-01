6b2fe524e282e2b5db2885a36f9d9504af883afd
sanjipun <iamsanjipun@gmail.com>
2022-02-01 21:53:54 +0545

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import About from '../components/About';
+import Contact from '../components/Contact';
 import SEO from '../components/SEO';
+import Skills from '../components/Skills';
+import Works from '../components/Works';
 
 const IndexPage = () => {
   return (
     <section>
       <SEO title="Sanji Pun | Frontend Engineer" />
-      <div className="max-w-8xl w-full mx-auto px-10">
+      <div className="max-w-8xl w-full mx-auto px-10 py-32">
         <h1 className="text-3xl text-red-500">Hello world!</h1>
       </div>
       <About />
+      <Works />
+      <Skills />
+      <Contact />
     </section>
   );
 };
