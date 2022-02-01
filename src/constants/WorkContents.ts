9d9994883cb94ce991221322ab4940dfe091091d
sanjipun <iamsanjipun@gmail.com>
2022-02-01 21:53:49 +0545

    add work constants

---
+++ src/constants/WorkContents.ts
@@ -0,0 +1,56 @@
+export const WORK_CONSTANTS = [
+    {
+        img: '/static/about.png',
+        title: 'exchange.nftyswap.io',
+        link: 'https://exchange.nftyswap.io',
+        subtitle: 'Exchange Royal Ecosystem tokens'
+    },
+    {
+        img: '/static/about.png',
+        title: 'royalprotocol.io',
+        link: 'https://royalprotocol.io',
+        subtitle: 'Official Site of Royal Protocol'
+    },
+    {
+        img: '/static/about.png',
+        title: 'royalarcaderewind.io',
+        link: 'https://exchange.nftyswap.io',
+        subtitle: 'Exchange Royal Ecosystem tokens'
+    },
+    {
+        img: '/static/about.png',
+        title: 'theroyaltimes.net',
+        link: 'https://theroyaltimes.net',
+        subtitle: 'Community Site dedicated to the royal protocol community.'
+    },
+    {
+        img: '/static/about.png',
+        title: 'gameficap.com',
+        link: 'https://gameficap.com',
+        subtitle: 'Gamefi token charts, prices and market Capitalization.'
+    },
+    {
+        img: '/static/about.png',
+        title: 'latidoleathers.com (maintainance)',
+        link: '',
+        subtitle: 'Ecommerce site for latido leathers products.'
+    },
+    {
+        img: '/static/about.png',
+        title: 'laxmipay.com.au',
+        link: 'https://laxmipay.com.au/',
+        subtitle: 'Remittance site to send money from Australia to Nepal.'
+    },
+    {
+        img: '/static/about.png',
+        title: 'etax.ausnep.com.au',
+        link: '',
+        subtitle: 'Australian Etax season.'
+    },
+    {
+        img: '/static/about.png',
+        title: 'crypto.supercoder.dev',
+        link: 'https://crypto.supercoder.dev/',
+        subtitle: 'Crypto portfolio.'
+    }
+]
