25351917c5245fd0483883886455d2f218381919
sanjipun <iamsanjipun@gmail.com>
2022-02-01 21:53:14 +0545

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,34 +8,36 @@ import useHamburger from '../store/Hamburger.store';
 const Header = () => {
   const { openMenu, setOpenMenu } = useHamburger();
   return (
-    <header className="max-w-8xl w-full mx-auto px-10 flex justify-between items-center py-10">
-      <h1 className="text-2xl uppercase tracking-widest">
-        <span className="border text-base py-2 px-3 border-black rounded-full">S</span> Sanji
-      </h1>
-      <div className="hidden md:inline-flex">
-        {HEADER_CONTENTS.map((header, index) => (
-          <Link
-            key={index}
-            to={header.path}
-            className="mx-2 p-2 text-sm flex flex-col items-end hover:bg-black hover:text-white 
+    <header className="max-w-8xl mx-auto">
+      <div className="flex justify-between items-center px-10 py-10">
+        <h1 className="text-2xl uppercase tracking-widest">
+          <span className="border text-base py-2 px-3 border-black rounded-full">S</span> Sanji
+        </h1>
+        <div className="hidden md:inline-flex">
+          {HEADER_CONTENTS.map((header, index) => (
+            <Link
+              key={index}
+              to={header.path}
+              className="mx-2 p-2 text-sm flex flex-col items-end hover:bg-black hover:text-white 
           transition-all duration-300 ease-in">
-            0{index + 1}
-            <span className="text-lg">{header.name}</span>
-          </Link>
-        ))}
+              0{index + 1}
+              <span className="text-lg">{header.name}</span>
+            </Link>
+          ))}
+        </div>
+        <CgMenuGridO
+          onClick={() => setOpenMenu()}
+          className={`${
+            openMenu && 'hidden'
+          } w-6 h-6 cursor-pointer md:hidden transition-all duration-300 ease-in`}
+        />
+        <CgClose
+          onClick={() => setOpenMenu()}
+          className={`${
+            !openMenu && 'hidden'
+          } w-6 h-6 cursor-pointer md:hidden transition-all duration-300 ease-in`}
+        />
       </div>
-      <CgMenuGridO
-        onClick={() => setOpenMenu()}
-        className={`${
-          openMenu && 'hidden'
-        } w-6 h-6 cursor-pointer md:hidden transition-all duration-300 ease-in`}
-      />
-      <CgClose
-        onClick={() => setOpenMenu()}
-        className={`${
-          !openMenu && 'hidden'
-        } w-6 h-6 cursor-pointer md:hidden transition-all duration-300 ease-in`}
-      />
     </header>
   );
 };
