e6e5f9b8d7b4cac7c725e1abc4a1d1b0ed0162f6
sanjipun <iamsanjipun@gmail.com>
2022-02-02 21:50:11 +0545

    works

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -4,7 +4,7 @@ import WorkCard from './WorkCard';
 
 const Works = () => {
   return (
-    <div className="max-w-8xl w-full mx-auto grid grid-cols-12 mt-32 px-10">
+    <div id="works" className="max-w-8xl w-full mx-auto grid grid-cols-12 py-44 px-10">
       <div className="col-span-12 md:col-span-2">
         <button>Works</button>
       </div>
