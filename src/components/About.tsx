d257eb9e6d49d62ffdc95b918c9c390a73c8be17
sanjipun <iamsanjipun@gmail.com>
2022-02-02 21:50:36 +0545

    about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="bg-black text-white">
-      <div className="max-w-8xl grid grid-cols-1 md:grid-cols-3 gap-20 w-full mx-auto px-10 py-32">
-        <div>
-          <button>About Me</button>
-        </div>
+    <div id="about" className="bg-black text-white">
+      <div className="max-w-8xl grid grid-cols-1 md:grid-cols-3 gap-20 w-full mx-auto px-10 py-44">
+        <div>About Me</div>
         <div className="grid grid-cols-1 gap-10">
           <p>
             I am Frontend Developer with 1 year+ experience in React JS. I can convert wireframes
@@ -16,7 +14,7 @@ const About = () => {
           <h1>Learning Blockchain and Solidity.</h1>
         </div>
         <div className="flex justify-center lg:justify-end">
-          <img src="/static/about.png" alt="About" />
+          <img src="about.png" alt="About" />
         </div>
       </div>
     </div>
