9825b8298e5df0070049b277e2102c89b00d83a9
sanjipun <iamsanjipun@gmail.com>
2022-02-02 21:50:27 +0545

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,35 +7,38 @@ import useHamburger from '../store/Hamburger.store';
 
 const Header = () => {
   const { openMenu, setOpenMenu } = useHamburger();
+  const handleScroll = (id: string) => {
+    document.getElementById(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  };
   return (
     <header className="max-w-8xl mx-auto">
       <div className="flex justify-between items-center px-10 py-10">
-        <h1 className="text-2xl uppercase tracking-widest">
-          <span className="border text-base py-2 px-3 border-black rounded-full">S</span> Sanji
-        </h1>
+        <h1 className="text-2xl uppercase tracking-widest">Sanji</h1>
         <div className="hidden md:inline-flex">
           {HEADER_CONTENTS.map((header, index) => (
-            <Link
+            <div
               key={index}
-              to={header.path}
+              onClick={() => handleScroll(header.path)}
               className="mx-2 p-2 text-sm flex flex-col items-end hover:bg-black hover:text-white 
           transition-all duration-300 ease-in">
               0{index + 1}
               <span className="text-lg">{header.name}</span>
-            </Link>
+            </div>
           ))}
         </div>
         <CgMenuGridO
           onClick={() => setOpenMenu()}
           className={`${
             openMenu && 'hidden'
-          } w-6 h-6 cursor-pointer md:hidden transition-all duration-300 ease-in`}
+          } w-6 h-6 cursor-pointer text-black md:hidden transition-all duration-300 ease-in`}
         />
         <CgClose
           onClick={() => setOpenMenu()}
           className={`${
             !openMenu && 'hidden'
-          } w-6 h-6 cursor-pointer md:hidden transition-all duration-300 ease-in`}
+          } w-6 h-6 cursor-pointer text-black md:hidden transition-all duration-300 ease-in`}
         />
       </div>
     </header>
