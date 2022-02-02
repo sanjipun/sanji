f000a2263090d708f8d07d55130062e2ff07da04
sanjipun <iamsanjipun@gmail.com>
2022-02-02 21:50:32 +0545

    contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="max-w-8xl mx-auto px-10 grid grid-cols-12 py-32">
+    <div id="contact" className="max-w-8xl mx-auto px-10 grid grid-cols-12 py-44">
       <div className="col-span-12 sm:col-span-2">Contact</div>
       <div className="col-span-12 sm:col-span-10 text-center text-4xl break-words">
         iamsanjaypun@gmail.com
