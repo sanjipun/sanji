e1b8bb08129589ed2d2845cac9f67c50d6e91fa3
sanjipun <iamsanjipun@gmail.com>
2022-02-02 21:49:58 +0545

    work contents

--- src/constants/WorkContents.ts
+++ src/constants/WorkContents.ts
@@ -1,54 +1,54 @@
 export const WORK_CONSTANTS = [
     {
-        img: '/static/about.png',
+        img: 'nfty.png',
         title: 'exchange.nftyswap.io',
         link: 'https://exchange.nftyswap.io',
         subtitle: 'Exchange Royal Ecosystem tokens'
     },
     {
-        img: '/static/about.png',
+        img: 'rp.svg',
         title: 'royalprotocol.io',
         link: 'https://royalprotocol.io',
         subtitle: 'Official Site of Royal Protocol'
     },
     {
-        img: '/static/about.png',
-        title: 'royalarcaderewind.io',
+        img: 're.svg',
+        title: 'royalarcade.io',
         link: 'https://exchange.nftyswap.io',
         subtitle: 'Exchange Royal Ecosystem tokens'
     },
+    //{
+    //    img: 'about.png',
+    //    title: 'theroyaltimes.net',
+    //    link: 'https://theroyaltimes.net',
+    //    subtitle: 'Community Site dedicated to the royal protocol community.'
+    //},
+    //{
+    //    img: 'about.png',
+    //    title: 'gameficap.com  (ongoing)',
+    //    link: 'https://gameficap.com',
+    //    subtitle: 'Gamefi token charts, prices and market Capitalization.'
+    //},
     {
-        img: '/static/about.png',
-        title: 'theroyaltimes.net',
-        link: 'https://theroyaltimes.net',
-        subtitle: 'Community Site dedicated to the royal protocol community.'
-    },
-    {
-        img: '/static/about.png',
-        title: 'gameficap.com',
-        link: 'https://gameficap.com',
-        subtitle: 'Gamefi token charts, prices and market Capitalization.'
-    },
-    {
-        img: '/static/about.png',
+        img: 'latido.jpeg',
         title: 'latidoleathers.com (maintainance)',
         link: '',
         subtitle: 'Ecommerce site for latido leathers products.'
     },
     {
-        img: '/static/about.png',
+        img: 'laxmipay.png',
         title: 'laxmipay.com.au',
         link: 'https://laxmipay.com.au/',
         subtitle: 'Remittance site to send money from Australia to Nepal.'
     },
+    //{
+    //    img: 'about.png',
+    //    title: 'etax.ausnep.com.au (maintainance)',
+    //    link: '',
+    //    subtitle: "Australia's online tax paying platform."
+    //},
     {
-        img: '/static/about.png',
-        title: 'etax.ausnep.com.au',
-        link: '',
-        subtitle: 'Australian Etax season.'
-    },
-    {
-        img: '/static/about.png',
+        img: 'crypto.png',
         title: 'crypto.supercoder.dev',
         link: 'https://crypto.supercoder.dev/',
         subtitle: 'Crypto portfolio.'
