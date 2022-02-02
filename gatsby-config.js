31a87b4a5193a8f3d59060844357682279081beb
sanjipun <iamsanjipun@gmail.com>
2022-02-02 21:56:00 +0545

    remove file system

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,6 @@ module.exports = {
     plugins: [
         'gatsby-plugin-postcss',
         'gatsby-plugin-react-helmet',
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `images`,
-                path: path.join(__dirname, `src`, `images`),
-            },
-        },
         'gatsby-plugin-sharp',
         'gatsby-transformer-sharp',
     ],
