8f593b3ea7b454b0279e7615d674d9be0ddfd970
sanjipun <iamsanjipun@gmail.com>
2022-02-02 21:49:45 +0545

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,12 @@ const IndexPage = () => {
   return (
     <section>
       <SEO title="Sanji Pun | Frontend Engineer" />
-      <div className="max-w-8xl w-full mx-auto px-10 py-32">
-        <h1 className="text-3xl text-red-500">Hello world!</h1>
+      <div id="home" className="max-w-8xl w-full mx-auto px-10 py-44">
+        <h1 className="text-3xl italic font-extralight text-black text-opacity-50">
+          Hello! I'm Sanji Pun
+        </h1>
+        <h1 className="text-5xl font-black my-10">React / Web3 Developer</h1>
+        <p className="text-3xl">Currently working at Royal Protocol</p>
       </div>
       <About />
       <Works />
