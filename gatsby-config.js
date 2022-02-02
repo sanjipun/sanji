3e85ce8daef7aa221d8528c0064ac926670fd42f
sanjipun <iamsanjipun@gmail.com>
2022-02-02 21:49:23 +0545

    config yarn and package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,20 @@
+const path = require(`path`)
+
 module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
         'gatsby-plugin-postcss',
-        'gatsby-plugin-react-helmet'
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: path.join(__dirname, `src`, `images`),
+            },
+        },
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
     ],
 }
