f55816c3fadeaa2e4551448e61a87a7416433ee8
sanjipun <iamsanjipun@gmail.com>
2022-02-04 17:42:59 +0545

    add item id on header

--- src/constants/HeaderContents.ts
+++ src/constants/HeaderContents.ts
@@ -1,19 +1,23 @@
-const HEADER_CONTENTS: { name: string; path: string }[] = [
+const HEADER_CONTENTS: { name: string; path: string; id: number }[] = [
     {
         name: 'Home',
         path: 'home',
+        id: 1
     },
     {
         name: 'About Me',
         path: 'about',
+        id: 2
     },
     {
         name: "Works",
-        path: 'works'
+        path: 'works',
+        id: 3
     },
     {
         name: "Skills",
-        path: "skills"
+        path: "skills",
+        id: 4
     },
 ];
 export default HEADER_CONTENTS;
